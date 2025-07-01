ecf6847a18203d47a44ad7a343e94fcc2749d7e8
SP-bi <jeffff_2000@yahoo.com>
2025-07-01 17:14:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.set_page_config(page_title="My Blog", layout="wide")
-st.title("📘 My Blog Embedded")
+st.title("📘 Performance by the Numbers: Industries in Focus")
 
 st.components.v1.iframe("https://power-bi-dev.blogspot.com/", height=10000)
