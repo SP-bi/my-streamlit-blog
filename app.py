a19bcc42c0749051bc57f7ad3675675db08c033e
SP-bi <jeffff_2000@yahoo.com>
2025-07-01 17:15:35 -0400

    app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ import streamlit as st
 st.set_page_config(page_title="My Blog", layout="wide")
 st.title("📘 Performance by the Numbers: Industries in Focus")
 
-st.components.v1.iframe("https://power-bi-dev.blogspot.com/", height=10000)
+st.components.v1.iframe("https://power-bi-dev.blogspot.com/", height=3000)
